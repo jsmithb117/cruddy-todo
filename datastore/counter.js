a7cf8ba4dfc6efcc88b017e4d69ebe877a13969d
jsmithb117 <jsmithb117@gmail.com>
2021-01-05 19:39:12 -0800

    Complete deleting a todo

--- datastore/counter.js
+++ datastore/counter.js
@@ -16,6 +16,7 @@ const zeroPaddedNumber = (num) => {
 };
 
 const readCounter = (callback) => {
+
   fs.readFile(exports.counterFile, (err, fileData) => {
     if (err) {
       callback(null, 0);
--- datastore/index.js
+++ datastore/index.js
@@ -40,13 +40,6 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  // var item = items[id];
-  // if (!item) {
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   items[id] = text;
-  //   callback(null, { id, text });
-  // }
   fs.readFile(`./test/testData/${id}.txt`, 'utf8', (err, todo) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
@@ -59,14 +52,9 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(`./test/testData/${id}.txt`, (err) => {
+    callback(err);
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
